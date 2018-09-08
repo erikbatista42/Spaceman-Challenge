a5e83fe0ebc0c930db771f052e8332d2505d7d37
ErikBatista <erikbatista42@gmail.com>
2018-09-07 18:18:33 -0700

    udpdate

--- Spaceman.py
+++ Spaceman.py
@@ -6,3 +6,22 @@ secureRandom = random.SystemRandom()
 
 pickedColor = secureRandom.choice(secretWordBank)
 print(pickedColor)
+
+letters = []
+# guessALetter = input("Guess a letter from A-Z")
+# letters.append(guessALetter)
+# guessAnotherLetter = input("Guess another letter from A-Z")
+# letters.append(guessAnotherLetter)
+# print(letters)
+# print(len(letters))
+
+while len(letters) < 7:
+    if len(letters) == 7:
+        print("Game Over")
+    else:
+        if len(letters) < 7:
+            guessALetter = input("Guess a letter from A-Z")
+            letters.append(guessALetter)
+            print(letters)
+        else:
+            print("Game Over")
