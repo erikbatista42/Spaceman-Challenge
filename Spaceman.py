d027a2343542d05649022db1129355d9b34952e1
ErikBatista <erikbatista42@gmail.com>
2018-09-12 15:33:21 -0700

    now tells the amount of guesses you have left

--- Spaceman.py
+++ Spaceman.py
@@ -1,5 +1,7 @@
 import random
 
+#loads the word from a text randomly
+
 def load_word():
    f = open('spaceman_words.txt', 'r')
    words_list = f.readlines()
@@ -9,14 +11,15 @@ def load_word():
    secret_word = random.choice(words_list)
    return secret_word
 
+#this is really nice and succinct! :D
 def get_available_letters(letters_guessed):
     # outputs remaining letters after each guess 
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    myAlphabet = list(alphabet)
-    for char in alphabet:
-        if char in letters_guessed:
+    alphabet = "abcdefghijklmnopqrstuvwxy"
+    myAlphabet = list(alphabet) # turns the alphabet string into characters
+    for char in alphabet: # loops over the alphabet string 
+        if char in letters_guessed:  # if characters in letters guessed exist, removed that character that exists
             myAlphabet.remove(char)
-    remainingLetters = ''.join(myAlphabet)
+    remainingLetters = "".join(myAlphabet) # 
     return remainingLetters
     
 def get_guessed_word(secret_word, letters_guessed):
@@ -28,8 +31,8 @@ def get_guessed_word(secret_word, letters_guessed):
         if char not in letters_guessed:
             charPosition = secretWordList.index(char)
             secretWordList.remove(char)
-            secretWordList.insert(charPosition, ' _ ')
-    strSoFar = ''.join(secretWordList)
+            secretWordList.insert(charPosition, " _ ") 
+    strSoFar = ''.join(secretWordList) # turns the characters back into a word 
     return strSoFar
 
 def is_word_guessed(secret_word, letters_guessed):
@@ -38,24 +41,46 @@ def is_word_guessed(secret_word, letters_guessed):
     # if the two lists are equal, the secret word has been guessed
     secretWordList = list(secret_word)
     noDuplicatesList = []
-    for char in secretWordList:
-        if char not in noDuplicatesList:
-            noDuplicatesList.append(char)
-    if sorted(noDuplicatesList) == sorted(letters_guessed):
+    for char in secretWordList: # loop characters inside the word list
+        if char not in noDuplicatesList: # if the character is not in the list, add the character to the list
+            noDuplicatesList.append(char) 
+    if sorted(noDuplicatesList) == sorted(letters_guessed):#
         return True
     else:
         return False
 
-def spaceman(secret_word):
-    letters_guessed = []
-    secretWordLen = len(secret_word)
-    print("The secret word is {0} letters long".format(secretWordLen)) 
+# def spaceman(secret_word):
+#     letters_guessed = []
+#     secretWordLen = len(secret_word)
+#     print('The secret word is {0} letters long'.format(secretWordLen)) 
+# test = 0
+# countDown = 7
+#     while len(letters_guessed) < 7: 
+#         print("You have 6 guesses left")
+#         guessLetter = input("Guess a letter: ").lower()
+#         letters_guessed.append(guessLetter)
+#         print("The secret word is ", get_guessed_word(secret_word, letters_guessed))
+#         print("The remaining letters are ", get_available_letters(letters_guessed))
 
-    while (is_word_guessed(secret_word, letters_guessed) is False):
-        guessLetter = input("Guess a letter: ").lower()
-        letters_guessed.append(guessLetter)
-        print("The secret word is ", get_guessed_word(secret_word, letters_guessed))
-        print("The remaining letters are ", get_available_letters(letters_guessed))
+# secret_word = load_word()
+# spaceman(load_word())
+def spaceman(secret_word):
+	totalGuesses = 7
+	letters_guessed = []
+	secretWordLen = len(secret_word)
+	print("The secret word is {0} letters long".format(secretWordLen)) 
+
+	while (is_word_guessed(secret_word, letters_guessed) is False):
+		guessLetter = input("Guess a letter: ").lower()
+		letters_guessed.append(guessLetter)
+		totalGuesses -= 1
+		print("The secret word is ", get_guessed_word(secret_word, letters_guessed))
+		print("You have {0} guesses left".format(totalGuesses))
+		print("The remaining letters are ", get_available_letters(letters_guessed))
 
 secret_word = load_word()
 spaceman(load_word())
+
+print("Game Over")
+
+
